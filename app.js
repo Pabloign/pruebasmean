33be6658551a27e7e4cc156bb619b7b03fe2ae6a
pabloign <pabloign@gmail.com>
2017-07-27 21:42:24 +0200

    adaptacion para heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express         = require("express"),
     mongoose        = require('mongoose');
 
 // Connection to DB
-mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+mongoose.connect(process.env.MONGOLAB_URI, function(err, res) {
   if(err) throw err;
   console.log('Connected to Database');
 });
@@ -38,9 +38,9 @@ tvshows.route('/tvshows/:id')
   .put(TVShowCtrl.updateTVShow)
   .delete(TVShowCtrl.deleteTVShow);
 
-app.use(tvshows);
+app.use(express.static(__dirname + '/'));
 
 // Start server
-app.listen(3000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("Node server running on http://localhost:3000");
 });
